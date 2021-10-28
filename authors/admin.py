ec19654369f9651a8db0c8bd78f8aaf47e0830d9
Lucas <lucaszeng99@gmail.com>
2021-10-27 21:52:47 -0600

    Add admin action so activating is easier

--- authors/admin.py
+++ authors/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from authors.models import Author, Follow, InboxObject
 
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    
+    # actions available on chosen Author objects
+    actions = ['activate_user_if_exists', 'deactivate_user_if_exists']
+
+    @admin.action(description='activate user if not already')
+    def activate_user_if_exists(self, request, queryset):
+        for author in queryset:
+            if author.user:
+                author.user.is_active = True
+                author.user.save()
+
+
+    @admin.action(description='deactivate user if not already')
+    def deactivate_user_if_exists(self, request, queryset):
+        for author in queryset:
+            if author.user:
+                author.user.is_active = False
+                author.user.save()
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Follow)
 admin.site.register(InboxObject)
