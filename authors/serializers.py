b0bb28650718168413c608fcc100715e441794bf
Student <zhpeng@ualberta.ca>
2021-11-25 23:29:21 -0500

    fix: modify to allow_blank

--- authors/serializers.py
+++ authors/serializers.py
@@ -17,9 +17,9 @@ class AuthorSerializer(serializers.ModelSerializer):
     # public id should be the full url
     id = serializers.CharField()
     displayName = serializers.CharField(
-        source='display_name', required=False, allow_null=True, blank=True)
+        source='display_name', required=False, allow_null=True, allow_blank=True)
     github = serializers.CharField(
-        source='github_url', required=False, allow_null=True, blank=True)
+        source='github_url', required=False, allow_null=True, allow_blank=True)
     url = serializers.URLField(required=False)
     host = serializers.URLField(required=False)
 
