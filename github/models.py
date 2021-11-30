d872db3b5cf6d85a8663a15561835e7918586bee
Student <zhpeng@ualberta.ca>
2021-11-30 01:37:06 -0500

    fix error

--- github/models.py
+++ github/models.py
@@ -22,12 +22,11 @@ class GithubEvent(models.Model):
         if github_event["type"] == GithubEvent.EventType.PUSH_EVENT:
             commits = github_event["payload"]["commits"]
             repo = "github.com/" + github_event["repo"]["name"]
-            content = f"[{self.username}]({self.url}) made 
-                    {len(commits)} commit(s) to [{repo}]({repo}) \n"
+            content = f"[{self.username}]({self.url}) made {len(commits)} commit(s) to [{repo}]({repo}): \n"
             for commit in commits:
                 sha = commit["sha"]
                 sha_url = sha.replace("repos/", "").replace("api", "www")
-                content += f"[{sha}]({sha_url}) + ": " + commit["message"]
+                content += f"[{sha}]({sha_url}): {commit["message"]}"
             self.event_content = content
             self.save()
     
